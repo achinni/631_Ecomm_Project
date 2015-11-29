b12512c59e56363c256eb4eb460317ef507e20fc
slavu1 <NaaNee@Srikanths-MacBook-Pro.local>
2015-11-29 13:44:16 -0500

    minor

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$_SESSION['user']="sample";
+	$_SESSION['user']="Guest";
 ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -18,11 +18,11 @@
 <!-- <nav class='navbar navbar-inverse'>
   <div class='container-fluid'>
     <div class='navbar-header'>
-      <a class='navbar-brand' href='home.html'>e-Mart</a>
+      <a class='navbar-brand' href='home.php'>e-Mart</a>
     </div>
     <div>
       <ul class='nav navbar-nav'>
-        <li class='active'><a href='home.php'>HOME</a></li>
+        <li class='active'><a href='home.php'><span class="glyphicon glyphicon-home"></span> HOME</a></li>
       </ul>
       <ul class='nav navbar-nav navbar-right'>
 		<!-- login popup for admin -->
@@ -45,77 +45,87 @@
 </nav> 
   
 <!-- body -->
-<div class='container'>
+<div class='container-fluid'>
 	<div class='jumbotron'>
 		<h1>Welcome to e-Mart</h1>      
 		<h4>Trending electronic mart!</h4>
 	</div>
-	<div class="col-sm-5">
-		<h2> Login </h2>
+	
+	<div class="col-md-4">
+		<h2> <span class="col-md-offset-4"><span class="glyphicon glyphicon-user"></span> Login</span> </h2> <br/>
 		<form action="user_login.php" method="post" class="form-horizontal" role="form">
 			<div class="form-group">
-				<label class="control-label col-sm-2" for="username">Username:</label>
-				<div class="col-sm-6">
+				<label class="control-label col-md-3" for="username">Username:</label>
+				<div class="col-md-8">
 				  <input type="text" class="form-control" name="username" id="username" placeholder="Enter username">
 				</div>
 			</div>
 			<div class="form-group">
-				<label class="control-label col-sm-2" for="pwd">Password:</label>
-				<div class="col-sm-6">
+				<label class="control-label col-md-3" for="pwd">Password:</label>
+				<div class="col-md-8">
 				  <input type="password" class="form-control" name="pwd" id="pwd" placeholder="Enter password">
 				</div>
 			</div>
 			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
+				<div class="col-md-offset-3 col-md-10">
 				  <button type="submit" class="btn btn-default">Sign-In</button>
 				  &emsp;<a>Forgot Password</a>
 				  <br/><br/>
-				  <button type = "button" class = "btn-lg btn-success">Continue as Guest</button>
 				</div>
+				
 			</div>
 		</form>
 	</div>
-	<div class="col-sm-7">
-		<h2> Sign up </h2>
+	
+	<div class="col-md-5">
+		<h2> <span class="col-md-offset-4"> Sign up </span> </h2> <br/>
 		<form class="form-horizontal" role="form">
 		  <div class="form-group">
-			<label class="control-label col-sm-2" for="username">Username:</label>
-			<div class="col-sm-6">
+			<label class="control-label col-md-3" for="username">Username:</label>
+			<div class="col-md-7">
 			  <input type="text" class="form-control" id="username" placeholder="Enter username">
 			</div>
 		  </div>
 		  <div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Password:</label>
-			<div class="col-sm-6">
+			<label class="control-label col-md-3" for="pwd">Password:</label>
+			<div class="col-md-7">
 			  <input type="password" class="form-control" id="pwd" placeholder="Enter password">
 			</div>
 		  </div>
 		  <div class="form-group">
-			<label class="control-label col-sm-2" for="repwd">Re-type Password:</label>
-			<div class="col-sm-6">
+			<label class="control-label col-md-3" for="repwd">Re-type Password:</label>
+			<div class="col-md-7">
 			  <input type="password" class="form-control" id="repwd" placeholder="Re-type password">
 			</div>
 		  </div>
 		  <div class="form-group">
-			<label class="control-label col-sm-2" for="fname">First Name:</label>
-			<div class="col-sm-6">
+			<label class="control-label col-md-3" for="fname">First Name:</label>
+			<div class="col-md-7">
 			  <input type="text" class="form-control" id="fname" placeholder="Enter First Name">
 			</div>
 		  </div>
 		  <div class="form-group">
-			<label class="control-label col-sm-2" for="lname">Last Name:</label>
-			<div class="col-sm-6">
+			<label class="control-label col-md-3" for="lname">Last Name:</label>
+			<div class="col-md-7">
 			  <input type="text" class="form-control" id="lname" placeholder="Enter Last Name">
 			</div>
 		  </div>
 		  
 		  <div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
+			<div class="col-md-offset-3 col-md-8">
 			  <button type="submit" class="btn btn-default">Submit</button>
 			</div>
 		  </div>
 		</form>
 	</div>
+	
+	<div class="col-md-3">
+		<div class="col-md-offset-0 col-md-12">
+			<h2> <span class="col-md-offset-0"> Guest Mode </span> </h2> <br/>
+			<button type = "button" class = "btn-lg btn-success">Proceed to Shopping</button>
+		</div>
+	</div>
+	
 </div>
 
   
