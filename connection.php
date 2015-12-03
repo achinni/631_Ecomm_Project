7583ca88a90563923ad6f3a6d2abcad5610eb6a7
slavu1 <NaaNee@public-72-158.emich.edu>
2015-12-02 21:01:10 -0500

    DB Created on Server

--- connection.php
+++ connection.php
@@ -1,14 +1,16 @@
 <?php
 session_start();
-$servername = "emart631group0.cfcsugvslzjq.us-west-2.rds.amazonaws.com:3306";
-$username = "master631";
+$servername = 'ecomm631.cfcsugvslzjq.us-west-2.rds.amazonaws.com';
+$username = 'master631';
 $password = "COSC_631!";
 $dbname = "631_group0";
+$port = 3306;
 
 // Create connection
-$connection = new mysqli($servername, $username, $password, $dbname);
+$connection = mysqli_connect($servername, $username, $password, $dbname, $port);
+
 // Check connection
 if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+    die('Connection failed: ' . $connection->connect_error);
 } 
 ?>
